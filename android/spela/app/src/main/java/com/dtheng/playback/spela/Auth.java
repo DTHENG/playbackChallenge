b27aa32b785e6258a27128b83aa0fd9548afcb32
Daniel Thengvall <fender5289@gmail.com>
2014-08-22 17:51:11 -0700

    setup android app auth flow

--- android/spela/app/src/main/java/com/dtheng/playback/spela/Auth.java
+++ android/spela/app/src/main/java/com/dtheng/playback/spela/Auth.java
@@ -4,21 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.dtheng.playback.spela.R;
 
-
-public class Player extends Base {
+public class Auth extends Base {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+        setContentView(R.layout.activity_auth);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.player, menu);
+        getMenuInflater().inflate(R.menu.auth, menu);
         return true;
     }
 
--- android/spela/app/src/main/java/com/dtheng/playback/spela/Base.java
+++ android/spela/app/src/main/java/com/dtheng/playback/spela/Base.java
@@ -1,24 +1,29 @@
 package com.dtheng.playback.spela;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class Player extends Base {
+public class Base extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+
+        if (this instanceof Auth) return;
+
+        Intent newIntent = new Intent(this, Auth.class);
+
+        this.startActivity(newIntent);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.player, menu);
+        getMenuInflater().inflate(R.menu.base, menu);
         return true;
     }
 
