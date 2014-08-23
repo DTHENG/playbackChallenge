e8e48491911a6d15a7b885ba424efaf89af63e4a
Daniel Thengvall <fender5289@gmail.com>
2014-08-22 18:17:46 -0700

    setup initial data flow for auth form

--- android/spela/app/src/main/java/com/dtheng/playback/spela/Auth.java
+++ android/spela/app/src/main/java/com/dtheng/playback/spela/Auth.java
@@ -2,16 +2,36 @@ package com.dtheng.playback.spela;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.dtheng.playback.spela.R;
 
 public class Auth extends Base {
+
+    private Button mButton;
+    private EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        mButton = (Button)findViewById(R.id.button2);
+        mEdit   = (EditText)findViewById(R.id.editText);
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("EditText", mEdit.getText().toString());
+                    }
+                });
     }
 
 
