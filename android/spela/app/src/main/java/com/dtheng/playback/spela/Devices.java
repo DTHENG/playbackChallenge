e637ac11df543b32e3acf82880fdbd5555771d55
Daniel Thengvall <fender5289@gmail.com>
2014-08-23 20:20:05 -0700

    finally rigged up devices activity

--- android/spela/app/src/main/java/com/dtheng/playback/spela/Devices.java
+++ android/spela/app/src/main/java/com/dtheng/playback/spela/Devices.java
@@ -2,34 +2,64 @@ package com.dtheng.playback.spela;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.ListActivity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by danielthengvall on 8/23/14.
  */
-public class Devices extends Base implements DevicesFragment.OnFragmentInteractionListener {
+public class Devices extends ListActivity {
+    TextView content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.devices_list);
+
+
+        //content = (TextView)findViewById(R.id.output);
+
+        //listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[] { "MacBook Pro", "iPhone 5S", "Galaxy S3"};
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+
+
+        // Assign adapter to List
+        setListAdapter(adapter);
+    }
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FrameLayout frame = new FrameLayout(this);
-        frame.setId(R.id.container);
-        setContentView(frame, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+    protected void onListItemClick(ListView l, View v, int position, long id) {
 
-        if (savedInstanceState == null) {
-            DevicesFragment newFragment = new DevicesFragment();
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.add(R.id.container, newFragment).commit();
-        }
+        super.onListItemClick(l, v, position, id);
+
+        finish();
+        /*
+        // ListView Clicked item index
+        int itemPosition     = position;
+
+        // ListView Clicked item value
+        String  itemValue    = (String) l.getItemAtPosition(position);
+
+        content.setText("Click : \n  Position :"+itemPosition+"  \n  ListItem : " +itemValue);
+          */
     }
-
-    public void onFragmentInteraction(Uri uri) { }
-
-    public void onFragmentInteraction(String str) { }
-
 }
