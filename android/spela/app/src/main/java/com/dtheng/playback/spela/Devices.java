11ecd339c75cdda8a8b82bc1541368d8e7250aad
Daniel Thengvall <fender5289@gmail.com>
2014-08-23 23:23:54 -0700

    support for device switching in android app

--- android/spela/app/src/main/java/com/dtheng/playback/spela/Devices.java
+++ android/spela/app/src/main/java/com/dtheng/playback/spela/Devices.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +15,36 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.dtheng.playback.spela.model.Device;
+import com.dtheng.playback.spela.model.Response;
+import com.dtheng.playback.spela.model.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by danielthengvall on 8/23/14.
  */
 public class Devices extends ListActivity {
     TextView content;
+
+    private List<Device> devices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +53,52 @@ public class Devices extends ListActivity {
         setContentView(R.layout.devices_list);
 
 
+        if (devices == null) {
+            InputStream inputStream = null;
+            String result = null;
+            try {
+
+                // create HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+
+                // make GET request to the given URL
+                HttpResponse httpResponse = httpclient.execute(
+                        new HttpGet("http://playback.dtheng.com/api?user=" +
+                                ((User) IO.get("user", new TypeToken<User>() {
+                                }.getType(), this)).id));
+
+                // receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // convert inputstream to string
+                if (inputStream != null)
+                    result = convertInputStreamToString(inputStream);
+
+            } catch (Exception e) {
+                //Log.d("InputStream", e.getLocalizedMessage());
+            }
+
+            if (result == null) {
+                devices = new ArrayList<Device>();
+                return;
+            }
+
+            Response response = new Gson().fromJson(result, new TypeToken<Response>() {}.getType());
+
+            devices = response.devices;
+        }
+
         //content = (TextView)findViewById(R.id.output);
 
         //listView = (ListView) findViewById(R.id.list);
-        String[] values = new String[] { "MacBook Pro", "iPhone 5S", "Galaxy S3"};
 
         // Define a new Adapter
         // First parameter - Context
         // Second parameter - Layout for the row
         // Third - the Array of data
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
+        ArrayAdapter<Device> adapter = new ArrayAdapter<Device>(this,
+                android.R.layout.simple_list_item_1, devices);
 
 
         // Assign adapter to List
@@ -51,6 +111,27 @@ public class Devices extends ListActivity {
 
         super.onListItemClick(l, v, position, id);
 
+
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://playback.dtheng.com/api");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("update", "true"));
+            nameValuePairs.add(new BasicNameValuePair("user", ((User) IO.get("user", new TypeToken<User>() {}.getType(), this)).id));
+            nameValuePairs.add(new BasicNameValuePair("device_id", devices.get(position) +""));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+
         finish();
         /*
         // ListView Clicked item index
@@ -62,4 +143,17 @@ public class Devices extends ListActivity {
         content.setText("Click : \n  Position :"+itemPosition+"  \n  ListItem : " +itemValue);
           */
     }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
 }
