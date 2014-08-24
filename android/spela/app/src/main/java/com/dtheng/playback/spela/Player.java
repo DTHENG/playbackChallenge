d7b7888f6e84bd92d3d6316e32051fc4a80ca7e4
Daniel Thengvall <fender5289@gmail.com>
2014-08-23 20:58:49 -0700

    rigged up android controls

--- android/spela/app/src/main/java/com/dtheng/playback/spela/Player.java
+++ android/spela/app/src/main/java/com/dtheng/playback/spela/Player.java
@@ -11,7 +11,28 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.dtheng.playback.spela.model.Response;
 import com.dtheng.playback.spela.model.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Base {
 
@@ -37,19 +58,82 @@ public class Player extends Base {
         previous = (Button)findViewById(R.id.previous);
         play = (Button)findViewById(R.id.play);
         pause = (Button)findViewById(R.id.pause);
+        next = (Button)findViewById(R.id.next);
 
         devices = (Button)findViewById(R.id.device);
 
         context = this;
 
         devices.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent devicesView = new Intent(context, Devices.class);
+                    context.startActivity(devicesView);
+                }
+            }
+        );
+
+        previous.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View view) {
-                        Intent devicesView = new Intent(context, Devices.class);
-                        context.startActivity(devicesView);
+                        update(true, false, true, context);
                     }
                 }
         );
+
+        play.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View view) {
+                    update(true, false, false, context);
+                }
+            }
+        );
+
+        pause.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View view) {
+                    update(false, false, false, context);
+                }
+            }
+        );
+
+        next.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View view) {
+                    update(true, true, false, context);
+                }
+            }
+        );
+    }
+
+    private static boolean update(boolean play, boolean next, boolean previous, ContextWrapper context) {
+
+
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://playback.dtheng.com/api");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("update", "true"));
+            nameValuePairs.add(new BasicNameValuePair("user", ((User) IO.get("user", new TypeToken<User>(){}.getType(), context)).id));
+            nameValuePairs.add(new BasicNameValuePair("state", play ? "PLAY" : "PAUSE"));
+            nameValuePairs.add(new BasicNameValuePair("next", next +""));
+            nameValuePairs.add(new BasicNameValuePair("previous", previous +""));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+            return true;
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+        return false;
     }
 
 
